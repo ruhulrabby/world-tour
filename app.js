20386b6eb92d722c6b8ca8f92ff8887ee701eb1e
ruhul rabby <ruhulalam1994@gmail.com>
2021-02-06 14:16:13 +0600

    another method using backtick and dynamic way

--- app.js
+++ app.js
@@ -8,13 +8,19 @@ for ( let i = 0; i < countries.length; i++) {
      const country = countries[i];
      const countryDiv = document.createElement('div');
      
-     const h3 = document.createElement('h3');
-     h3.innerText = country.name;
-     countryDiv.appendChild(h3);
+    //  const h3 = document.createElement('h3');
+    //  h3.innerText = country.name;
+    //  countryDiv.appendChild(h3);
 
-     const p = document.createElement('p');
-     p.innerText = country.capital
-     countryDiv.appendChild(p);
+    //  const p = document.createElement('p');
+    //  p.innerText = country.capital
+    //  countryDiv.appendChild(p);
+
+    const countryInfo = `
+        <h3>${country.name}</h3>
+        <p>${country.capital}</p>
+    `
+    countryDiv.innerHTML = countryInfo;
 
      countriesDiv.appendChild(countryDiv);
     }
