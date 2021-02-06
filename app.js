6b88e55a33aea2b6e538028a9fcbff6cd900576d
ruhul rabby <ruhulalam1994@gmail.com>
2021-02-06 15:56:37 +0600

    call dynamix api and show counrty details

--- app.js
+++ app.js
@@ -9,8 +9,27 @@ fetch('https://restcountries.eu/rest/v2/all')
             const countryInfo = `
                  <h3 class = 'country-name' >${country.name}</h3>
                  <p>${country.capital}</p>
+                 <button onclick="displaCountryDetail('${country.name}')">Details</button>
             `;
             countryDiv.innerHTML = countryInfo;
             countriesDiv.appendChild(countryDiv);
      });
-}
+    }
+
+
+    const displaCountryDetail = name => {
+        const url = `https://restcountries.eu/rest/v2/name/${name}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => renderCountryInfo(data[0]))
+    }
+
+    const renderCountryInfo = country => {
+        const countryDiv = document.getElementById('countryDetail');
+        countryDiv.innerHTML =`
+            <h1>${country.name} </h1>
+             <p>${country.population}</p>
+             <p>Area: ${country.area}</p>
+             <img src="${country.flag}">
+        `
+    }
