ea7c73fb6cd197308b17b562423edc2b373d5c64
ruhul rabby <ruhulalam1994@gmail.com>
2021-02-06 15:28:00 +0600

    using for each method

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
 fetch('https://restcountries.eu/rest/v2/all')
 .then(res => res.json())
 .then(data => displayCountries(data));
-
-const displayCountries = (countries) =>{
-    const countriesDiv = document.getElementById('countries');
-for ( let i = 0; i < countries.length; i++) {
-     const country = countries[i];
-     const countryDiv = document.createElement('div');
-     countryDiv.className = 'country'
-     
-    //  const h3 = document.createElement('h3');
-    //  h3.innerText = country.name;
-    //  countryDiv.appendChild(h3);
-
-    //  const p = document.createElement('p');
-    //  p.innerText = country.capital
-    //  countryDiv.appendChild(p);
-
-    const countryInfo = `
-        <h3 class = 'country-name' >${country.name}</h3>
-        <p>${country.capital}</p>
-    `
-    countryDiv.innerHTML = countryInfo;
-
-     countriesDiv.appendChild(countryDiv);
-    }
+     const displayCountries = (countries) =>{
+     const countriesDiv = document.getElementById('countries');
+     countries.forEach(country => {
+            const countryDiv = document.createElement('div');
+            countryDiv.className = 'country'
+            const countryInfo = `
+                 <h3 class = 'country-name' >${country.name}</h3>
+                 <p>${country.capital}</p>
+            `;
+            countryDiv.innerHTML = countryInfo;
+            countriesDiv.appendChild(countryDiv);
+     });
 }
